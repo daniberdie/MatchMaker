e30c3070b89b84f62677173787735efcfab85c60
DaniBerdie <berdie97@hotmail.com>
2020-03-21 12:59:48 +0100

    Ajustar pantalla de inici sessió i registre

---
+++ app/src/main/java/com/example/matchmaker/AccesActivity.java
@@ -0,0 +1,15 @@
+package com.example.matchmaker;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AccesActivity extends Activity {
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_acces);
+    }
+
+}
