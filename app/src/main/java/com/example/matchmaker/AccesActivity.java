d58846aa34db33cb645d8bfd70c4d96cc44f52f2
DaniBerdie <berdie97@hotmail.com>
2020-03-28 20:58:53 +0100

    Pantalla ajuda, accedir i registrar

--- app/src/main/java/com/example/matchmaker/AccesActivity.java
+++ app/src/main/java/com/example/matchmaker/AccesActivity.java
@@ -1,15 +1,57 @@
 package com.example.matchmaker;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class AccesActivity extends Activity {
 
+    private Button help_button, login_button, register_button;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_acces);
+
+        help_button     = findViewById(R.id.ConfigAccessActivity);
+        login_button    = findViewById(R.id.Login);
+        register_button = findViewById(R.id.Register);
+
+        help_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                moveToHelpActivity();
+            }
+        });
+        login_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                moveToLoginActivity();
+            }
+        });
+        register_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                moveToRegisterActivity();
+            }
+        });
+    }
+
+    private void moveToRegisterActivity() {
+        Intent intent = new Intent(AccesActivity.this, RegisterActivity.class);
+        startActivity(intent);
+    }
+
+    private void moveToLoginActivity() {
+        Intent intent = new Intent(AccesActivity.this, LoginActivity.class);
+        startActivity(intent);
+    }
+
+    private void moveToHelpActivity() {
+        Intent intent = new Intent(AccesActivity.this, HelpActivity.class);
+        startActivity(intent);
     }
 
 }
---
+++ app/src/main/java/com/example/matchmaker/LoginActivity.java
@@ -0,0 +1,76 @@
+package com.example.matchmaker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private Button  back_button, enter_button;
+    private EditText user, password;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        back_button = findViewById(R.id.back_button_login);
+        enter_button = findViewById(R.id.enter_button);
+
+        user = findViewById(R.id.user);
+        password = findViewById(R.id.password);
+
+        enter_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                moveToMainActivity();
+            }
+        });
+        back_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                moveToAccessActivity();
+            }
+        });
+    }
+
+    private void moveToMainActivity() {
+
+        if(validateLogin(this)){
+            //TODO: Mantenir sessió iniciada
+            //Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+            //startActivity(intent);
+        }else{
+            Toast toast = Toast.makeText(this,getString(R.string.invalid_user_pass), Toast.LENGTH_LONG);
+            toast.show();
+        }
+
+
+    }
+
+    private boolean validateLogin(Context context) {
+
+        boolean ret = false;
+
+        SharedPreferences sharedPref = context.getSharedPreferences(getString(R.string.shared_preferences), Context.MODE_PRIVATE);
+        String default_user = sharedPref.getString(getString(R.string.saved_user),"admin");
+        String default_password = sharedPref.getString(getString(R.string.saved_password),"admin");
+
+        if(user.getText().toString() == default_user && password.getText().toString() == default_password){
+            ret = true;
+        }
+
+        return ret;
+    }
+
+    private void moveToAccessActivity() {
+        finish();
+    }
+}
